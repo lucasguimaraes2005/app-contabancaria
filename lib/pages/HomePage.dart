4c0b8d0e29309dc62422c3a6311047c3d804e1a0
lucasguimaraes2005 <lucaagui123@gmail.com>
2024-06-17 22:16:57 -0300

    feat: iniciando implementação da homepage

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -12,10 +13,50 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
+  String _nomeUsuario = '';
+  String _emailUsuario = '';
+  bool _temContaBancaria = false;
 
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text('Home', style: TextStyle(fontWeight: FontWeight.bold),),
-    Text('Search', style: TextStyle(fontWeight: FontWeight.bold),)
+  @override
+  void initState() {
+    super.initState();
+    _buscarDadosUsuario();
+  }
+
+  void _buscarDadosUsuario() async {
+
+    var url = Uri.parse('http://192.168.1.14:8080/user');
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var dadosUsuario = json.decode(response.body);
+      setState(() {
+        _nomeUsuario = dadosUsuario['nome'];
+        _emailUsuario = dadosUsuario['email'];
+        _temContaBancaria = dadosUsuario['contasBancarias'].isNotEmpty;
+      });
+    } else {
+      print('Falha ao buscar dados do usuário');
+    }
+  }
+
+  static List<Widget> _widgetOptions(String nome, String email, bool temConta) => <Widget>[
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Bem vindo! $nome', style: TextStyle(fontWeight: FontWeight.bold),),
+        Text('O que temos para hoje?', style: TextStyle(fontWeight: FontWeight.bold),),
+        Text('Informações da minha conta', style: TextStyle(fontWeight: FontWeight.bold),),
+        Text('Email: $email', style: TextStyle(fontWeight: FontWeight.bold),),
+        Text(temConta ? 'Você tem conta bancária e pode conferir informações dela na sessão "Minha conta"' : 'Você ainda não tem uma conta bancária! Crie uma na sessão "Minha conta"', style: TextStyle(fontWeight: FontWeight.bold),),
+      ],
+    ),
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("teste")
+      ],
+    )
   ];
 
   void _onItemTapped(int index) {
@@ -29,11 +70,10 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text("GreenBank"),
+        title: Text(widget.title),
       ),
       body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+        child: _widgetOptions(_nomeUsuario, _emailUsuario, _temContaBancaria).elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
@@ -50,7 +90,6 @@ class _MyHomePageState extends State<MyHomePage> {
         selectedItemColor: Colors.greenAccent,
         onTap: _onItemTapped,
       ),
-
     );
   }
 }
